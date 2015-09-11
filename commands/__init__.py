e0bf9658bf3c153842aef000b4e5f8f3a66d35ed
uppfinnarn <uppfinnarn@gmail.com>
2015-09-11 18:23:00 +0200

    Patch merging

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,11 +1,13 @@
 import missing
 import crcpatch
 import blacklist
+import merge
 import compile_
 
 commands = [
 	('missing', missing),
 	('crcpatch', crcpatch),
 	('blacklist', blacklist),
+	('merge', merge),
 	('compile', compile_),
 ]
---
+++ commands/merge.py
@@ -0,0 +1,22 @@
+import sys
+import json
+from util import load
+
+desc = u"Imports translated lines"
+
+def add_arguments(parser):
+	parser.add_argument('lang')
+	parser.add_argument('infile')
+
+def run(data, args):
+	pd = load(args.infile)
+	d = {}
+	for item in data:
+		if not item['ctx'] in d:
+			d[item['ctx']] = {}
+		d[item['ctx']][item['orig']] = item
+	
+	for item in pd:
+		if item[args.lang]:
+			d[item['ctx']][item['orig']][args.lang] = item[args.lang]
+	return data
