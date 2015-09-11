8538e413dc7d9ca521d37602afd1571baa9ac1ef
uppfinnarn <uppfinnarn@gmail.com>
2015-09-11 18:10:07 +0200

    More boilerplate

--- commands/crcpatch.py
+++ commands/crcpatch.py
@@ -1,5 +1,6 @@
 import json
 from binascii import crc32
+from util import load
 
 desc = u"Applies a CRC32-keyed patch"
 
@@ -8,9 +9,7 @@ def add_arguments(parser):
 	parser.add_argument('patchfile')
 
 def run(data, args):
-	with open(args.patchfile, 'rU') as f:
-		pdata = json.load(f)
-	
+	pdata = load(args.patchfile)
 	newdata = data
 	for i, d in enumerate(data):
 		crc = str(crc32(d['orig'].encode('utf-8')) & 0xffffffff)
